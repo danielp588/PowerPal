d1e41c336b8327e4d9d80af45d358cef18a88e62
Daniel P <danielpodvornyy@gmail.com>
2023-06-13 12:10:21 +0300

    separate server side
    server side is in differrent repo, removed unneeded files.

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import TabNavigator from './navigation/TabNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import UserContextProvider from './contexts/UserContext';
+
 export default function App() {
   return (
     <UserContextProvider>
